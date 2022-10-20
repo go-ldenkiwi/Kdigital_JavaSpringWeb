9d3653503efd817c9b52fe8fb4ea16c2aadb7018
JY <wnduq297@gmail.com>
2022-10-20 15:23:10 +0900

    first commit

---
+++ src/main/java/com/joongbu/WebSNS/WebSnsApplication.java
@@ -0,0 +1,13 @@
+package com.joongbu.WebSNS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSnsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joongbu/WebSNS/WebSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joongbu.WebSNS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
