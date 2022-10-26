058695068cebe7526decba7d8446e68015927093
Ori0li <gogo5125@naver.com>
2022-10-26 17:45:41 +0900

    오류 수정

--- src/main/java/com/joongbu/WebSNS/mapper/GroupMapper.java
+++ src/main/java/com/joongbu/WebSNS/mapper/GroupMapper.java
@@ -2,8 +2,10 @@ package com.joongbu.WebSNS.mapper;
 
 import java.util.List;
 
-import com.joongbu.WebSNS.dto.GroupDto;
+import org.apache.ibatis.annotations.Mapper;
 
+import com.joongbu.WebSNS.dto.GroupDto;
+@Mapper
 public interface GroupMapper {
 	List<GroupDto> list();
 	GroupDto detail(int groupNo);
